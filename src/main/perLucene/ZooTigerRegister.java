1c3cc975facded99a255ca1ca5b3546682c3850f
Apostolis Xekoukoulotakis <apostolis@Fingolfin.fritz.box>
2013-01-04 18:15:35 +0100

    more work

--- src/main/perLucene/ZooTigerRegister.java
+++ src/main/perLucene/ZooTigerRegister.java
@@ -27,6 +27,7 @@ import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.ACL;
 import java.util.ArrayList;
+import org.apache.zookeeper.data.Stat;
 
 public class ZooTigerRegister extends ZooAbstract
 {
@@ -109,36 +110,66 @@ public class ZooTigerRegister extends ZooAbstract
 
 // the maximum size is in Gigabytes
 
+//condition 0 only indexing
+//          1 fully operational
+
     protected void registerTiger (long added, String location, int maximum_size,
                                   int split_size)
     {
 
         try {
-
+            Stat stat = new Stat ();
             int nServers =
-                ByteBuffer.wrap (zoo.
-                                 getData ("/tiger/Servers", false,
-                                          null)).getInt ();
+                ByteBuffer.wrap (zoo.getData ("/tiger/Servers", false,
+                                              stat)).getInt ();
+
             String path = "/Tiger/Servers/" + Integer.toString (nServers + 1);
             zoo.create (path, null, acl, CreateMode.PERSISTENT);
 
             long upper_boundary =
-                ByteBuffer.
-                wrap (zoo.getData ("/tiger/last_upper_boundary", false,
-                                   null)).getLong ();
+                ByteBuffer.wrap (zoo.
+                                 getData ("/tiger/last_upper_boundary", false,
+                                          null)).getLong ();
             zoo.create (path + "/up",
                         ByteBuffer.allocate (8).putLong (added +
                                                          upper_boundary).array
                         (), acl, CreateMode.PERSISTENT);
 
             zoo.create (path + "/down",
-                        ByteBuffer.allocate (8).
-                        putLong (upper_boundary).array (), acl,
-                        CreateMode.PERSISTENT);
+                        ByteBuffer.allocate (8).putLong (upper_boundary).
+                        array (), acl, CreateMode.PERSISTENT);
 
-            zoo.create (path + "/location",
+            zoo.create (path + "/replicas",
+                        ByteBuffer.allocate (4).putInt (1).array
+                        (), acl, CreateMode.PERSISTENT);
+
+
+            zoo.create (path + "/replicas/1",
                         location.getBytes ("UTF-8"), acl,
                         CreateMode.PERSISTENT);
+
+            zoo.create (path + "/replicas/1/notifications",
+                        null, acl, CreateMode.PERSISTENT);
+
+            zoo.create (path + "/replicas/1/condition",
+                        ByteBuffer.allocate (4).putInt (0).array (), acl,
+                        CreateMode.PERSISTENT);
+
+//the minimum of all the max_sizes of the replicas
+
+            zoo.create (path + "/maximum_size",
+                        ByteBuffer.allocate (4).putInt (maximum_size).array (),
+                        acl, CreateMode.PERSISTENT);
+
+            zoo.create (path + "/split_size",
+                        ByteBuffer.allocate (4).putInt (maximum_size).array (),
+                        acl, CreateMode.PERSISTENT);
+
+
+            // update the number of servers
+            zoo.setData ("/tiger/Servers",
+                         ByteBuffer.allocate (4).putInt (nServers).array (),
+                         stat.getVersion ());
 
 
         } catch (KeeperException e) {
@@ -164,4 +195,79 @@ public class ZooTigerRegister extends ZooAbstract
 
 
     }
+
+
+    protected void replicateTiger (int whichInterval, String location,
+                                   int maximum_size)
+    {
+
+        try {
+            Stat stat = new Stat ();
+            String path = "/Tiger/Servers/" + Integer.toString (whichInterval);
+
+            int nReplica =
+                ByteBuffer.wrap (zoo.getData (path + "/replicas", false,
+                                              stat)).getInt ();
+
+
+
+            zoo.create (path + "/replicas/" + Integer.toString (nReplica + 1),
+                        location.getBytes ("UTF-8"), acl,
+                        CreateMode.PERSISTENT);
+
+            zoo.create (path + "/replicas/" + Integer.toString (nReplica + 1) +
+                        "/notifications", null, acl, CreateMode.PERSISTENT);
+
+            zoo.create (path + "/replicas/" + Integer.toString (nReplica + 1) +
+                        "/condition",
+                        ByteBuffer.allocate (4).putInt (0).array (), acl,
+                        CreateMode.PERSISTENT);
+
+// update the replicas
+
+            zoo.setData (path + "/replicas",
+                         ByteBuffer.allocate (4).putInt (nReplica).array (),
+                         stat.getVersion ());
+
+
+            zoo.create (path + "/maximum_size",
+                        ByteBuffer.allocate (4).putInt (maximum_size).array (),
+                        acl, CreateMode.PERSISTENT);
+
+            int max =
+                ByteBuffer.wrap (zoo.getData (path + "/maximum_size", false,
+                                              stat)).getInt ();
+            if (maximum_size < max) {
+
+                zoo.setData (path + "/maximum_size",
+                             ByteBuffer.allocate (4).
+                             putInt (maximum_size).array (),
+                             stat.getVersion ());
+            }
+
+
+
+
+        }
+        catch (KeeperException e) {
+            if (!e.code ().equals (KeeperException.Code.OK)) {
+
+                System.out.println
+                    ("zookeeper client exited with error code " +
+                     e.code ().toString ());
+                System.out.println (e.toString ());
+                System.exit (-1);
+
+
+            }
+        }
+        catch (Exception e) {
+            System.out.println ("zookeeper client interrupted");
+            System.out.println (e.toString ());
+            System.exit (-1);
+        }
+
+
+    }
+
 }
